14171d03e62e4535353b537a1d9a72d543c9799a
David du Colombier <0intro@gmail.com>
2013-03-13 16:36:55 +0000

    proc-note-wired
    i'm not sure whose change this originally was, since my systems
    have got it, but not some others. essentially, if a process is
    wired to a given processor, respect that, and don't try to
    move it through "cooperative scheduling".
    
    Author(s): forsyth
    
    Reference: 022-proc-note-wired
    Date: Wed Mar 13 16:36:55 GMT 2013
    Signed-off-by: jmk

--- port/proc.c
+++ port/proc.c
@@ -412,7 +412,7 @@ ready(Proc *p)
 		return;
 	}
 
-	if(up != p)
+	if(up != p && (p->wired == nil || p->wired == m))
 		m->readied = p;	/* group scheduling */
 
 	updatecpu(p);
@@ -497,6 +497,7 @@ runproc(void)
 
 	/* cooperative scheduling until the clock ticks */
 	if((p=m->readied) && p->mach==0 && p->state==Ready
+	&& (p->wired == nil || p->wired == m)
 	&& runq[Nrq-1].head == nil && runq[Nrq-2].head == nil){
 		skipscheds++;
 		rq = &runq[p->priority];
