17157a22bbf9747e06499ab6736c94bfa1974c96
David du Colombier <0intro@gmail.com>
2013-05-07 14:52:59 +0000

    little-sync-up-0
    1) bigger Runes: port/lib.h, port/latin1.c
    2) random.c moves to port with a spelling and grammar fix;
    3) install rule in k10/mkfile
    
    Author(s): various
    
    Reference: 038-little-sync-up-0
    Date: Tue May  7 14:52:59 GMT 2013
    Signed-off-by: jmk

--- port/lib.h
+++ port/lib.h
@@ -34,10 +34,12 @@ extern	int	tokenize(char*, char**, int);
 
 enum
 {
-	UTFmax		= 3,		/* maximum bytes per rune */
-	Runesync	= 0x80,		/* cannot represent part of a UTF sequence */
+	UTFmax		= 4,		/* maximum bytes per rune */
+	Runesync	= 0x80,		/* cannot represent part of a UTF sequence (<) */
 	Runeself	= 0x80,		/* rune and UTF sequences are the same (<) */
 	Runeerror	= 0xFFFD,	/* decoding error in UTF */
+	Runemax		= 0x10FFFF,	/* 24 bit rune */
+	Runemask	= 0x1FFFFF,	/* bits used by runes (see grep) */
 };
 
 /*
